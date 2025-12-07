e5233a52a7d25c277e5d9a36f9073c2b1b0b543d
matthew-hajec <matthew.hajec02@gmail.com>
2025-12-07 00:35:55 -0500

    move svgs into seperate components

--- app/components/Accordian.tsx
+++ app/components/Accordian.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ChevronDown } from "./icons/ChevronDown";
 
 type AccordianProps = {
   title: React.ReactNode;
@@ -25,21 +26,10 @@ export default function Accordian(props: AccordianProps) {
       >
         {props.title}
 
-        <svg
+        <ChevronDown
           className={`w-5 h-5 transform transition-transform duration-200 ${isOpen ? "rotate-180" : "rotate-0"
             }`}
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
+        />
       </button>
       <div
         className={`grid transition-all duration-300 ease-in-out ${isOpen
--- app/features/Search.tsx
+++ app/features/Search.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import TextInput from "~/components/TextInput";
 import AutocompleteInput from "~/components/AutocompleteInput";
 import Accordian from "~/components/Accordian";
+import { Spinner } from "~/components/icons/Spinner";
+import { LocationMarker } from "~/components/icons/LocationMarker";
 
 interface SearchParams {
   latitude: number;
@@ -147,50 +149,12 @@ export default function Search({
           >
             {isGettingLocation ? (
               <>
-                <svg
-                  className="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
+                <Spinner className="animate-spin -ml-1 mr-2 h-4 w-4" />
                 Getting Location...
               </>
             ) : (
               <>
-                <svg
-                  className="mr-2 h-4 w-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
+                <LocationMarker className="mr-2 h-4 w-4" />
                 Use Location
               </>
             )}
